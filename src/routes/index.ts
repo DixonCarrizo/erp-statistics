bd2f769c2697b6223c1fb92f53705ff57957621f
dixon <dixon.carrizo.daca@gmail.com>
2019-06-13 14:07:16 -0500

    Health check

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 import asteriskRoutes from './asterisk'
 import dolibarr from './dolibarr'
 
@@ -6,5 +6,10 @@ export const router = Router()
 
 router.use('/asterisk', asteriskRoutes)
 router.use('/dolibarr', dolibarr)
+
+router.get('/health', (req: Request, res: Response) => {
+  console.log(req.query)
+  res.sendStatus(200)
+})
 
 export default router
