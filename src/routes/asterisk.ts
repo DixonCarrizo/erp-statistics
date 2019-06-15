458b5b2909a6167a3c792327936c6966b20ecdfa
dixon <dixon.carrizo.daca@gmail.com>
2019-06-15 16:34:05 -0500

    bulk

--- src/routes/asterisk.ts
+++ src/routes/asterisk.ts
@@ -77,7 +77,25 @@ router.get('/load', login, async (req: IAuthorizedRequest, res: Response) => {
 
   let responses = await Promise.all(promises)
 
-  await asterisStatistisk.bulkCreate(responses.flat())
+  if (!Array.prototype.flat) {
+    Array.prototype.flat = function(depth) {
+      var flattend = [];
+      (function flat(array, depth) {
+        for (let el of array) {
+          if (Array.isArray(el) && depth > 0) {
+            flat(el, depth - 1); 
+          } else {
+            flattend.push(el);
+          }
+        }
+      })(this, Math.floor(depth) || 1);
+      return flattend;
+    };
+  }
+  let bulk = responses.flat()
+  console.log(bulk)
+
+  await asterisStatistisk.bulkCreate(bulk)
 
   res.send('Listo')
 
